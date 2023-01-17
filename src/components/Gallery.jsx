c6135bf2aaa373af6cbaccab933a1a097e11e4b9
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-01-17 21:51:55 +0100

    Working on gallery sass

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -7,12 +7,31 @@ import product_five from "../../src/images/image-product-4-thumbnail.jpg";
 
 function Gallery() {
   return (
-    <div>
-      <img src={product_one} alt="A pair of shoes with an orange background" />
-      <img src={product_two} alt="A pair of shoes with an orange background" />
-      <img src={product_three} alt="A pair of shoes with an orange background" />
-      <img src={product_four} alt="A pair of shoes with an orange background" />
-      <img src={product_five} alt="A pair of shoes with an orange background" />
+    <div className="gallery-container">
+      <div className="gallery-container-product">
+        <img src={product_one} alt="A pair of shoes with an orange background" />
+        <div className="gallery-container-thumbnails">
+          <div className="thumbnail-one">
+            <img src={product_two} alt="A pair of shoes with an orange background" className="selected-thumbnail" />
+            <div className="thumbnail-one-overlay"></div>
+          </div>
+
+          <div className="thumbnail-two">
+            <img src={product_three} alt="A pair of shoes with an orange background" />
+            <div className="thumbnail-two-overlay"></div>
+          </div>
+
+          <div className="thumbnail-three">
+            <img src={product_four} alt="A pair of shoes with an orange background" />
+            <div className="thumbnail-three-overlay"></div>
+          </div>
+
+          <div className="thumbnail-four">
+            <img src={product_five} alt="A pair of shoes with an orange background" />
+            <div className="thumbnail-four-overlay"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
